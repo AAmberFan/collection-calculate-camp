d23b5596f65a896570795304bda9de8cd5abc866
AAmberFan <filuwan@gmail.com>
2018-04-03 16:20:53 -0400

    all passed finally

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -2,16 +2,26 @@
 
 function get_integer_interval_2(number_a, number_b) {
   //在这里写入代码
+  var result = new Array();
   var diff = number_b - number_a;
-
+  if(diff === 0){
+    if(number_a%2===0){
+    return [number_a];
+  }
+    else{
+      return result;
+    }
+  }
   var sign = diff/Math.abs(diff);
+
   if(number_a%2 ==1){
     number_a += sign;
   }
   if(number_b%2 == 1){
     number_b-=sign;
   }
-  var result = new Array();
+
+
   result.push(number_a);
   while(number_a!=number_b){
     number_a +=sign*2;
@@ -19,6 +29,7 @@ function get_integer_interval_2(number_a, number_b) {
   }
 return result;
 }
-// var a = get_integer_interval_2(10, 1);
-// console.log(a);
+// var collection_a = [2, 4, 6, 8, 10];
+//   var collection_b = [10, 8, 6, 4, 2];
+//   console.log(get_integer_interval_2(10,10));
 module.exports = get_integer_interval_2;
